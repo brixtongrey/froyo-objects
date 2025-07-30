750f8f61091c7f6a2f4cf3589c15843d08adf2af
Brixtongrey <brixtonaruda@gmail.com>
2025-07-30 08:07:58 -0400

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// creating an object to fill with color count
+
+const colors = ["vanilla", "vanilla", "vanilla", "strawberry", "coffee", "coffee"];
+// vanilla: 3, strawberry: 1, coffee: 2
+
+
+
