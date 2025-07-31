c08a50f197367203f5c17d9cb0226b4b0ae8c113
Brixtongrey <brixtonaruda@gmail.com>
2025-07-31 11:17:34 -0400

    feat: added object for flavor count

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-
 // creating an object to fill with color count
 
-const colors = ["vanilla", "vanilla", "vanilla", "strawberry", "coffee", "coffee"];
+const flavors = [
+  "vanilla",
+  "vanilla",
+  "vanilla",
+  "strawberry",
+  "coffee",
+  "coffee",
+];
 // vanilla: 3, strawberry: 1, coffee: 2
 
+// creating an object to fill with flavor count
+const frequencyCounter = {};
+for (let i = 0; i < flavors.length; i++) {
+  const flavor = flavors[i];
+  console.log("flavor", flavor);
 
+  console.log(frequencyCounter);
 
+  if (flavor in frequencyCounter) {
+    frequencyCounter[flavor] += 1;
+  } else {
+    frequencyCounter[flavor] = 1;
+  }
+}
 
-
+console.log("flavor", frequencyCounter);
 
 // how to create prompt message from Stats workshop
 const userInputString = prompt(
   "Please submit your froyo flavor.",
   "vanilla, strawberry, coffee"
 );
-
